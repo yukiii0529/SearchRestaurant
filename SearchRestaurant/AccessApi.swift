80460d11dd497e1f583136a8c21d0bd05814817f
yukiii0529 <57751716+yukiii0529@users.noreply.github.com>
2021-04-13 10:59:39 +0900

    add api relation

---
+++ SearchRestaurant/AccessApi.swift
@@ -0,0 +1,17 @@
+//
+//  AccessApi.swift
+//  SearchRestaurant
+//
+//  Created by 田中勇輝 on 2021/04/13.
+//
+
+import UIKit
+
+class AccessApi: UIViewController {
+    
+    let accessApi = "0c86b34f889617a2"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
